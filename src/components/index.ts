41710e0dabb358caa69f6d716255c4fbf85b0687
Rickson Yang <rixin.yang.2001@gmail.com>
2023-06-01 11:14:07 -0400

    feat: create landing page

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,3 @@
 export { Header } from "./header";
-export { Logo } from "./logo";
 export { Main } from "./main";
-export { Button } from "./button";
-export { Cards } from "./cards";
 export { Footer } from "./footer";
